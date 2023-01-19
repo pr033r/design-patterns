c627804efa179937a6ebd3fc6b3f8f53d4098d37
Adam Lasák <lasak.ad@gmail.com>
2023-01-19 23:13:01 +0100

    Regular and lazy singletons.

---
+++ design-patterns-book/build/singleton/basic-singleton.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+* Instance is created just right in definition of the static attribute,
+* where the reference will be stored.
+* Other implementation: there won't be any getInstance() method and the
+* instance property will be defined as a 'public'. This will prevent
+* calling 'getter' method many times (performance) - use it only in
+* an emergency cases - tell that using 'global readonly' property is good
+* for the performance if we call it only 3-times in the rest of the code
+* is a nonsense.
+* */
+class BasicSingleton {
+    // the private constructor will defend creating new instances
+    constructor(testString = 'Tester attribute') {
+        this.testString = testString;
+    }
+    // basic Factory method returning a singleton instance
+    static getInstance() {
+        return this.instance;
+    }
+    toString() {
+        return this.testString;
+    }
+}
+BasicSingleton.instance = new BasicSingleton();
---
+++ design-patterns-book/src/singleton/lazy-singleton.ts
@@ -0,0 +1,38 @@
+/*
+* Lazy Load Singleton - means, that the instance of the class is created
+* when we first call the getInstance() method. So it's not initialized
+* right in the declaration (because if initiate class, but we actually won't
+* need it at all in the program). Then we'll use lazy singleton. It's also
+* useful for cases where creating the instance has performance issues. So we'll
+* create it, only when we need it.
+* */
+class LazySingleton {
+  private static lazy: LazySingleton;
+
+  // private for preventing crating a new instance
+  private constructor() {
+    console.log('Creating a new instance of the LazySingleton');
+  }
+
+  public static getInstance(): LazySingleton {
+    console.log('Request for getting LazySingleton instance.');
+    if (this.lazy === undefined) {
+      console.log('%c Instance does not exist, let\'s create a new one', 'color: red');
+      this.lazy = new LazySingleton();
+    }
+    console.log('-> Returning created instance.');
+    return this.lazy;
+  }
+
+  public static isInstanceExist(): boolean {
+    return this.lazy !== undefined;
+  }
+
+  public static test() {
+    console.log(`LazySingleton exists: ${this.isInstanceExist()}`);
+    const newLazy1 = this.getInstance();
+    console.log(`LazySingleton exists: ${this.isInstanceExist()}`);
+    const newLazy2 = this.getInstance();
+    console.log(`Fetched LazySingletons are ${(newLazy1 == newLazy2) ? 'same' : 'different'}`);
+  }
+}
